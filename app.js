3f2972f3dd153e21f02f3116376f6dc93454a923
saifkhan29 <79800224+saifkhan29@users.noreply.github.com>
2022-02-15 22:03:15 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,210 @@
+const Pab = document.querySelectorAll('.pab1')
+
+Pab.forEach(a_head => {
+    a_head.innerText = "PAB DAAOA Theory"
+    a_head.href = "http://meet.google.com/pjc-dckf-vka"
+})
+
+const Pab_b1_tut = document.querySelectorAll('.pab_b1_tut')
+
+Pab_b1_tut.forEach(a_head => {
+    a_head.innerText = "PAB DAAOA:B1 Tutorial"
+    a_head.href = "http://meet.google.com/pjc-dckf-vka"
+})
+
+const Pab_b2_tut = document.querySelectorAll('.pab_b2_tut')
+
+Pab_b2_tut.forEach(a_head => {
+    a_head.innerText = "PAB DAAOA:B2 Tutorial"
+    a_head.href = "http://meet.google.com/pjc-dckf-vka"
+})
+
+const Pab_b3_tut = document.querySelectorAll('.pab_b3_tut')
+
+Pab_b3_tut.forEach(a_head => {
+    a_head.innerText = "PAB DAAOA:B3 Tutorial"
+    a_head.href = "http://meet.google.com/pjc-dckf-vka"
+})
+
+//////////////////////////////
+
+const ppg = document.querySelectorAll('.ppg1')
+
+ppg.forEach(ppg2 => {
+    ppg2.innerText = "PPG MLADL Theory"
+    ppg2.href = "http://meet.google.com/pjc-dckf-vka"
+})
+
+////////////////////////////////
+
+
+const rsj = document.querySelectorAll('.rsj1')
+
+rsj.forEach(rsj2 => {
+    rsj2.innerText = 'RSJ MLADL:B1 Tutorial';
+    rsj2.href = 'https://meet.google.com/yko-bysx-ksm'
+})
+
+const rsj3 = document.querySelectorAll('.rsj3')
+
+rsj3.forEach(rsj2 => {
+    rsj2.innerText = 'RSJ MLADL:B3 Tutorial';
+    rsj2.href = 'https://meet.google.com/yko-bysx-ksm'
+})
+
+const rsj_b2_tut = document.querySelectorAll('.rsj_b2_tut')
+
+rsj_b2_tut.forEach(rsj2 => {
+    rsj2.innerText = 'RSJ MLADL:B2 Tutorial';
+    rsj2.href = 'https://meet.google.com/yko-bysx-ksm'
+})
+
+
+////////////////////////////////
+
+const psd = document.querySelectorAll('.psd1')
+
+psd.forEach(psd2 => {
+    psd2.innerText = 'PSD WTACC:B3 Tutorial'
+    psd2.href = 'https://meet.google.com/vsi-duid-upf'
+})
+
+const psd_b2_tut = document.querySelectorAll('.psd_b2_tut')
+
+psd_b2_tut.forEach(psd2 => {
+    psd2.innerText = 'PSD WTACC:B2 Tutorial'
+    psd2.href = 'https://meet.google.com/vsi-duid-upf'
+})
+
+///////////////////////////////
+
+const pabB2 = document.querySelectorAll('.pabB2')
+
+pabB2.forEach(pab2 => {
+    pab2.innerText = 'PAB DAAOA:B2 Tutorial'
+    pab2.href = 'https://meet.google.com/apr-rqcy-wnt'
+})
+
+////////////////////////
+
+const ars1 = document.querySelectorAll('.ars1')
+
+ars1.forEach(ars => {
+    ars.innerText = 'ARS WTACC Theory'
+    ars.href = 'https://meet.google.com/cez-ztbd-vdn'
+})
+
+
+///////////////////////////////
+
+const djj1 = document.querySelectorAll('.djj1')
+
+djj1.forEach(djj => {
+    djj.innerText = 'DJJ SP Theory'
+    djj.href = 'https://meet.google.com/azd-rpcy-stn'
+})
+
+///////////////////////////////
+
+const djj_b1_lab = document.querySelectorAll('.djj_b1_lab')
+const djj_b2_lab = document.querySelectorAll('.djj_b2_lab')
+const djj_b3_lab = document.querySelectorAll('.djj_b3_lab')
+
+djj_b1_lab.forEach(lab => {
+    lab.innerText = 'DJJ SP:B1 Lab 1201'
+    lab.href = 'https://meet.google.com/azd-rpcy-stn'
+})
+
+djj_b2_lab.forEach(lab => {
+    lab.innerText = 'DJJ SP:B2 Lab 1201'
+    lab.href = 'https://meet.google.com/azd-rpcy-stn'
+})
+
+djj_b3_lab.forEach(lab => {
+    lab.innerText = 'DJJ SP:B3 Lab 1201'
+    lab.href = 'https://meet.google.com/azd-rpcy-stn'
+})
+
+///////////////////////////////
+
+const prk_b1_lab = document.querySelectorAll('.prk_b1_lab')
+const prk_b2_lab = document.querySelectorAll('.prk_b2_lab')
+const prk_b3_lab = document.querySelectorAll('.prk_b3_lab')
+
+prk_b1_lab.forEach(lab => {
+    lab.innerText = 'PRK WTACC:B1 Lab 1202'
+    lab.href = 'https://meet.google.com/uer-fwau-ywy'
+})
+
+prk_b2_lab.forEach(lab => {
+    lab.innerText = 'PRK WTACC:B2 Lab 1202'
+    lab.href = 'https://meet.google.com/uer-fwau-ywy'
+})
+
+prk_b3_lab.forEach(lab => {
+    lab.innerText = 'PRK WTACC:B3 Lab 1202'
+    lab.href = 'https://meet.google.com/uer-fwau-ywy'
+})
+
+///////////////////////////////
+
+const pab_b1_lab = document.querySelectorAll('.pab_b1_lab')
+const pab_b2_lab = document.querySelectorAll('.pab_b2_lab')
+const pab_b3_lab = document.querySelectorAll('.pab_b3_lab')
+
+pab_b1_lab.forEach(lab => {
+    lab.innerText = 'PAB DAAOA:B1 Lab 1316A'
+    lab.href = 'https://meet.google.com/apr-rqcy-wnt'
+})
+
+pab_b2_lab.forEach(lab => {
+    lab.innerText = 'PAB DAAOA:B2 Lab 1316A'
+    lab.href = 'https://meet.google.com/apr-rqcy-wnt'
+})
+
+pab_b3_lab.forEach(lab => {
+    lab.innerText = 'PAB DAAOA:B3 Lab 1316A'
+    lab.href = 'https://meet.google.com/apr-rqcy-wnt'
+})
+
+///////////////////////////////
+
+const ppg_b1_lab = document.querySelectorAll('.ppg_b1_lab')
+const ppg_b2_lab = document.querySelectorAll('.ppg_b2_lab')
+const ppg_b3_lab = document.querySelectorAll('.ppg_b3_lab')
+
+ppg_b1_lab.forEach(lab => {
+    lab.innerText = 'PPG MLADL:B1 Lab 1308'
+    lab.href = 'http://meet.google.com/pjc-dckf-vka'
+})
+
+ppg_b2_lab.forEach(lab => {
+    lab.innerText = 'PPG MLADL:B2 Lab 1308'
+    lab.href = 'http://meet.google.com/pjc-dckf-vka'
+})
+
+ppg_b3_lab.forEach(lab => {
+    lab.innerText = 'PPG MLADL:B3 Lab 1308'
+    lab.href = 'http://meet.google.com/pjc-dckf-vka'
+})
+
+///////////////////////////////
+
+const ars_b1_lab = document.querySelectorAll('.ars_b1_lab')
+const ars_b2_lab = document.querySelectorAll('.ars_b2_lab')
+const ars_b3_lab = document.querySelectorAll('.ars_b3_lab')
+
+ars_b1_lab.forEach(lab => {
+    lab.innerText = 'ARS WTACC:B2 Lab 1202'
+    lab.href = 'https://meet.google.com/cez-ztbd-vdn'
+})
+
+ars_b2_lab.forEach(lab => {
+    lab.innerText = 'ARS WTACC:B2 Lab 1202'
+    lab.href = 'https://meet.google.com/cez-ztbd-vdn'
+})
+
+ars_b3_lab.forEach(lab => {
+    lab.innerText = 'ARS WTACC:B3 Lab 1202'
+    lab.href = 'https://meet.google.com/cez-ztbd-vdn'
+})
